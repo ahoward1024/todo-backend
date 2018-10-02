1828563f481c479ef5ea9abc6575aa4dccb5c28e
Alex Howard <ahoward1024@gmail.com>
2018-10-02 12:27:20 -0600

    Create production environment variables parsing
    The server now parses for the environment variables:
    - MONGO_USER
    - MONGO_PASS
    - MONGO_DOMAIN
    - MONGO_PORT
    - MONGO_HEROKU
    - MONGO_DB_NAME
    - MONGO_DB_COLLECTION
    to connect to the MongoDB instance (in our case mLab) and formats these
    into a standard MongoDB URI.
    The output should be:
    mongodb://<user>:<password>@<domain>:<port>/<heroku>

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import constants
+import os
 
-db = {
-  constants.MONGO_HOST: 'localhost',
-  constants.MONGO_PORT: 27017
+auth = {
+    constants.MONGO_USER: os.environ[constants.MONGO_USER],
+    constants.MONGO_PASS: os.environ[constants.MONGO_PASS],
+    constants.MONGO_DOMAIN: os.environ[constants.MONGO_DOMAIN],
+    constants.MONGO_PORT: int(os.environ[constants.MONGO_PORT]),
+    constants.MONGO_HEROKU: os.environ[constants.MONGO_HEROKU],
+    constants.MONGO_DB_NAME: os.environ[constants.MONGO_DB_NAME],
+    constants.MONGO_DB_COLLECTION: os.environ[constants.MONGO_DB_COLLECTION]
 }
