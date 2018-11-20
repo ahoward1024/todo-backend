9621974e3bd434b2cf0def54c7680c9e2589a1f0
Alex Howard <ahoward1024@gmail.com>
2018-11-20 14:16:47 -0700

    Change MongoDB authentication to use less environment variables
    The having so many environment variables to configure MongoDB
    authentication was getting unweildy so it now just consists of
    a MONGODB_AUTH string with the full string of the `mongouri` eg:
    `mongodb://user:pass@domain.com:1234/instance` and a MONGO_DB_NAME
    and MONGO_DB_COLLECTION for the name of the database and collection
    respectively

--- server/config.py
+++ server/config.py
@@ -2,11 +2,7 @@ import os
 from server import constants
 
 auth = {
-    constants.MONGO_USER: os.environ[constants.MONGO_USER],
-    constants.MONGO_PASS: os.environ[constants.MONGO_PASS],
-    constants.MONGO_DOMAIN: os.environ[constants.MONGO_DOMAIN],
-    constants.MONGO_PORT: int(os.environ[constants.MONGO_PORT]),
-    constants.MONGO_HEROKU: os.environ[constants.MONGO_HEROKU],
     constants.MONGO_DB_NAME: os.environ[constants.MONGO_DB_NAME],
-    constants.MONGO_DB_COLLECTION: os.environ[constants.MONGO_DB_COLLECTION]
+    constants.MONGO_DB_COLLECTION: os.environ[constants.MONGO_DB_COLLECTION],
+    constants.MONGO_AUTH: os.environ[constants.MONGO_AUTH]
 }
